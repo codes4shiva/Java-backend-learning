9513394858f13f0e86f9e8621a3b88fc17f3ac4a
codes4shiva <shivann2006@gmail.com>
2026-06-27 13:05:52 +0530

    Object creation and scope types

---
+++ Spring_framework/Object_Creation_and_Scopes/src/main/java/com/shivanshu/Alien.java
@@ -0,0 +1,11 @@
+package com.shivanshu;
+
+public class Alien {
+    int age;
+    public Alien() {
+        System.out.println("Alien object created");
+    }
+    public void code(){
+        System.out.println("Conding..");
+    }
+}
---
+++ Spring_framework/Object_Creation_and_Scopes/src/main/java/com/shivanshu/Laptop.java
@@ -0,0 +1,10 @@
+package com.shivanshu;
+
+public class Laptop {
+    public Laptop() {
+        System.out.println("Laptop Object created");
+    }
+    public void code(){
+        System.out.println("Coded the project..");
+    }
+}
